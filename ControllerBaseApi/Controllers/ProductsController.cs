5923c49f46aa2ac35d067bce75c97d0b24b35c97
ME <ME@DESKTOP-R4IEKC0>
2026-02-16 22:01:36 +0200

    Add project files.

---
+++ ControllerBaseApi/Controllers/ProductsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ControllerBaseApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController] //APiController attribute is used to indicate that this controller will be used to handle API requests.
+                    //It provides features such as automatic model validation and binding source inference.
+    public class ProductsController : ControllerBase
+    {
+
+        [HttpGet]
+        public string Get()
+        {
+            return "Product01";
+        }
+    }
+}
---
+++ ControllerBaseApi/Program.cs
@@ -0,0 +1,22 @@
+namespace ControllerBaseApi
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddControllers();
+
+
+            var app = builder.Build();
+
+
+            app.MapControllers();
+
+            app.MapGet("/", () => "Hello World!");
+
+            app.Run();
+        }
+    }
+}
